95c48beb4a4d06087168c1571d0dd105fab5b294
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2019-10-25 17:07:44 -0400

    Testcase: AutoTest: enable testing a single case; case 2-2: fix RAW of beq

--- testcase/test.sh
+++ testcase/test.sh
@@ -2,12 +2,17 @@
 
 DIFF=vimdiff
 DIFFEND=""
-if ! command -v vimdiff 2>1 >/dev/null ; then
+if ! command -v vimdiff 2>&1 >/dev/null ; then
     DIFF="diff --side-by-side --color=always"
     DIFFEND="|less"
 fi
 
-for i in */; do
+testcase=$1
+if [[ "$testcase" == "" ]]; then
+    testcase=$(ls -d -- */)
+fi
+
+for i in $testcase; do
     echo -e "=============================================================\n>>>>  $i"
     cd $i
     ../MIPS_pipeline
@@ -15,8 +20,8 @@ for i in */; do
     DMEM_SAME=$?
     diff -wq RFresult*
     RF_SAME=$?
-    [[ $DMEM_SAME == 1 || $RF_SAME == 1 ]] && echo 'Result differs from answer. Press Enter to view.' && read
-    [[ $DMEM_SAME == 1 ]] && eval "$DIFF dmemresult* $DIFFEND"
-    [[ $RF_SAME == 1 ]] && eval "$DIFF RFresult* $DIFFEND"
+    [[ "$DMEM_SAME" == "1" || "$RF_SAME" == "1" ]] && echo 'Result differs from answer. Press Enter to view.' && read
+    [[ "$DMEM_SAME" == "1" ]] && eval "$DIFF dmemresult* $DIFFEND"
+    [[ "$RF_SAME" == "1" ]] && eval "$DIFF RFresult* $DIFFEND"
     cd ..
 done
